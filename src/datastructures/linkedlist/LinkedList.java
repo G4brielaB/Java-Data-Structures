e4ed6d75599602daea5969c96aca5091e551b751
G4brielaB <G4brielaB@users.noreply.github.com>
2024-03-28 10:09:29 +0200

    linked list constructor

---
+++ src/datastructures/linkedlist/LinkedList.java
@@ -0,0 +1,23 @@
+package datastructures.linkedlist;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+}
