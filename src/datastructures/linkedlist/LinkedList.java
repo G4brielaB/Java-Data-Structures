35532907dcccbc534867e721982671c238e61ddf
G4brielaB <G4brielaB@users.noreply.github.com>
2024-03-28 12:14:58 +0200

    addes print and append methods

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -9,6 +9,11 @@ public class LinkedList {
         int value;
         Node next;
 
+        Node() {
+            this.value = 0;
+            this.next = null;
+        }
+
         Node(int value) {
             this.value = value;
         }
@@ -19,5 +24,37 @@ public class LinkedList {
         head = newNode;
         tail = newNode;
         length = 1;
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
     }
 }
--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -2,6 +2,11 @@ package datastructures.linkedlist;
 
 public class Main {
     public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
 
+        myLinkedList.append(5);
+        myLinkedList.append(6);
+
+        myLinkedList.printList();
     }
 }
